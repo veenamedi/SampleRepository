ed62d6e8b8b0179639d374014d23a691766461b1
veena medi <veenamedi@gmail.comgit config --global user.email veenamedi@gmail.comgit config --global user.email veenamedi@gmail.com>
2021-10-04 08:36:00 -0700

    updated checkbox

--- src/main/java/SeleniumSessions/CheckboxUsingStream.java
+++ src/main/java/SeleniumSessions/CheckboxUsingStream.java
@@ -1,9 +1,30 @@
 package SeleniumSessions;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class CheckboxUsingStream {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/");
+		System.out.println(driver.getTitle());
+		List<WebElement> choicelist = driver.findElements(By.className("comboTreeInputBox"));
+		
+		for(int i=0;i<choicelist.size();i++) {
+			String text=choicelist.get(i).getText();
+			System.out.println(text);
+		}
+		
 
 	}
 
